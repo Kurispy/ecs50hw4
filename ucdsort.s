4aae6b98ca77191055ea17a2cc62e1436aa197f4
CSquared <tech@ucdavis.edu>
2012-11-18 21:42:58 -0800

    Implemented pthread_create loop

--- ucdsort.s
+++ ucdsort.s
@@ -1,16 +1,37 @@
+#sizeof(pthread_t) = 4
+
+.equ MAX_THREADS, 10
+
 .data
 
-
+threads:
+  .rept MAX_THREADS
+  .long 0
+  .endr
 
 .text
 
 .globl ucdsort
 
 ucdsort: #(int *x, int n, int nth)
-  
-  #create nth threads
   #split array x into roughly equal sized chunks based on nth and assign to threads
-  #find min/max in each thread
+  movl $0, %ebx #thread counter
+  
+createloop:
+  movl $threads, %ecx
+  .rept 4
+  addl %ebx, %ecx #&threads[i]
+  .endr
+  push %ecx
+  push $0 #This might not work
+  push $sort
+  push %ebx
+  call pthread_create
+
+  incl %ebx
+  cmpl 4(%ebp), %ebx
+  jnz createloop #leave loop when nth threads have been created
+  
   #BARRIER combine results to get overall min/max
 
   #split range mx-mn into roughly equal sized chunks based on nth and assign to threads
@@ -24,3 +45,10 @@ ucdsort: #(int *x, int n, int nth)
 
   #???
   #Profit
+
+minmax: #assume %ecx = size of chunk; %edx = remainder; 
+  #4(%ebp) = thread number
+  #find min/max in each thread
+
+sort:
+  
