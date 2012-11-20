f79238f53716b7b2bda17afe4a96427197e23287
CSquared <tech@ucdavis.edu>
2012-11-19 22:52:51 -0800

    part2

--- ucdsort.s
+++ ucdsort.s
@@ -4,6 +4,27 @@
 
 .data
 
+nthreads:
+  .long 0
+
+csize:
+  .long 0
+
+remainder:
+  .long 0
+
+array:
+  .long 0
+
+min:
+  .long 0
+
+max:
+  .long 0
+
+range:
+  .long 0
+
 threads:
   .rept MAX_THREADS
   .long 0
@@ -14,25 +35,33 @@ threads:
 .globl ucdsort
 
 ucdsort: #(int *x, int n, int nth)
+  movl 4(%esp), nthreads
+  movl 12(%esp), %eax
+  movl %eax, array
   #split array x into roughly equal sized chunks based on nth and assign to threads
+  movl 8(%esp), %eax
+  idivl nthreads #standard chunk size is now in %eax, with remainder in %edx
+  movl %eax, csize
+  movl %edx, remainder
   movl $0, %ebx #thread counter
   
 createloop:
   movl $threads, %ecx
-  .rept 4
+  shll $2, %ebx
   addl %ebx, %ecx #&threads[i]
-  .endr
-  push %ecx
-  push $0 #This might not work
-  push $sort
-  push %ebx
+  shrl $2, %ebx
+  pushl %ecx #pthread_t *thread
+  pushl $0 #pthread_attr_t NULL
+  pushl $sort #pointer to function
+  pushl %ebx #argument passed to function
   call pthread_create
+  addl $16, %esp
 
   incl %ebx
-  cmpl 4(%ebp), %ebx
+  cmpl 4(%esp), %ebx
   jnz createloop #leave loop when nth threads have been created
   
-  #BARRIER combine results to get overall min/max
+  #gminmax combine results to get overall min/max
 
   #split range mx-mn into roughly equal sized chunks based on nth and assign to threads
   #CRITICAL SECTION in each thread determine which elements are in its range as well as how many of
@@ -46,9 +75,72 @@ createloop:
   #???
   #Profit
 
-minmax: #assume %ecx = size of chunk; %edx = remainder; 
-  #4(%ebp) = thread number
-  #find min/max in each thread
+worker: 
+  #4(%esp) = thread number
+  movl csize, %eax
+  imull 4(%esp)
+  movl %eax, %esi #esi holds start location of chunk in array
+  movl csize, %edi #edi now holds length 
+  cmpl $0, 4(%esp)
+  jnz notthread0
+  addl remainder, %edi #edi now holds length (thread 0)
+notthread0:
+  movl $0, %eax #counter
+  movl $0, %edx #min
+  movl $0, %ebp #max
+  movl array, %ecx
+  shll $2, %esi
+  addl %esi, %ecx #ecx is now the start location of the chunk
+  shrl $2, %esi
+loop:
+  movl (%ecx, %eax, 4), %ebx  #ebx holds current element
+  cmpl %ebx, %edx
+  jl newmin
+  cmpl %ebx, %ebp
+  jg newmax
 
-sort:
+  incl %eax
+  cmpl %eax, %edi
+  jnz loop
+  jmp gminmax
+
+newmin:
+  movl %ebx, %edx
+  incl %eax
+  cmpl %eax, %edi
+  jnz loop
+  jmp gminmax
+
+newmax:
+  movl %ebx, %ebp
+  incl %eax
+  cmpl %eax, %edi
+  jnz loop
+  jmp gminmax
+
+gminmax:
+  lock cmpl %edx, min
+  jl newgmin
+  lock cmpl %ebp, max
+  jg newgmax
+  jmp barrier
+
+newgmin:
+  lock movl %edx, min
+  jmp barrier
+
+newgmax:
+  lock movl %ebp, max
+  jmp barrier
+
+barrier:
+  
+
+part2:
+  movl max, %eax
+  subl min, %eax
+  incl %eax #eax = range
+  idivl nthreads
+  movl %eax, csize
+  movl %edx, remainder
   
